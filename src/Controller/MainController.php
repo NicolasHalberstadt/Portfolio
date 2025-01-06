ccf63a868bd783c6eef9755f9cacb07d0510fd2c
Halberstadt Nicolas <halberstadtnicolas@gmail.com>
2025-01-06 18:21:18 +0100

    handle contact form

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,55 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index()
     {
+        $form = $this->createForm(ContactType::class);
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/contact', name: 'app_contact', methods: ['POST'])]
+    public function contact(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $email = (new Email())
+                ->from($data['email'])
+                ->to('contact@nicolashalberstadt.com')
+                ->subject('Nouvelle demande de contact')
+                ->text("Message : " . $data["message"]);
+
+            $mailer->send($email);
+
+            $message = "J'ai bien reçu votre demande de contact pour la création d'un site " . $data["type"] . " et je reviendrai vers vous dans les plus brefs délais.";
+
+            return $this->json(['status' => 'success', 'message' => $message]);
+        }
+
+        $errors = [];
+        foreach ($form->all() as $child) {
+            if (!$child->isValid()) {
+                $errors[$child->getName()][] = (string) str_replace('ERROR:', '', $child->getErrors(false));
+            }
+        }
+
+        return $this->json(['status' => 'error', 'errors' => $errors]);
     }
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Form;
+
+use PixelOpen\CloudflareTurnstileBundle\Type\TurnstileType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['placeholder' => 'Jane', 'class' => 'form-label'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Ce champ ne peut pas être vide.']),
+                    new Length(['min' => 1])
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Doe', 'class' => 'form-label'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Ce champ ne peut pas être vide.']),
+                    new Length(['min' => 1])
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['placeholder' => 'Où puis-je vous répondre ?', 'class' => 'form-label'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Ce champ ne peut pas être vide.']),
+                    new Length(['min' => 1]),
+                    new Email(['message' => 'Ce champ doit contenir une adresse email valide.'])
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message',
+                'attr' => ['placeholder' => 'Parlez-moi de votre projet web !', 'class' => 'form-label'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Ce champ ne peut pas être vide.']),
+                    new Length(['min' => 49, 'minMessage' => 'Votre message doit contenir 50 charactères minimum'])
+                ]
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'E-commerce' => 'e-commerce',
+                    'Vitrine' => 'vitrine',
+                ],
+                'attr' => ['class' => 'form-label'],
+                'expanded' => true,
+                'multiple' => false,
+                'label' => 'Vous êtes interessé par un site :',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Vous devez selectionner 1 choix']),
+                ]
+            ])
+            ->add('security', TurnstileType::class, ['attr' => ['data-action' => 'contact', 'data-theme' => 'dark'], 'label' => false])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
