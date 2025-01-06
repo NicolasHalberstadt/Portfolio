b1cbd147b2d2f6e079157d03156c259e556890e5
Halberstadt Nicolas <halberstadtnicolas@gmail.com>
2025-01-06 18:05:30 +0100

    installing bunldes

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
+    PixelOpen\CloudflareTurnstileBundle\PixelOpenCloudflareTurnstileBundle::class => ['all' => true],
 ];
