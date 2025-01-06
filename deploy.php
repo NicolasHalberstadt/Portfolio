05712c7e819caac964ed20aed766eb66cc456627
Halberstadt Nicolas <halberstadtnicolas@gmail.com>
2025-01-06 18:19:45 +0100

    deploy file

---
+++ deploy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Config
+
+set('repository', 'git@github.com:NicolasHalberstadt/Portfolio.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('Portfolio')
+    ->set('remote_user', 'u416463632')
+    ->set('deploy_path', '/home/u416463632/domains/nicolashalberstadt.com/public_html');
+
+// Tasks
+
+task('deploy:assets', function () {
+    // Navigue vers le répertoire du projet et compile les assets
+    run('cd {{release_path}} && php bin/console sass:build');
+    run('cd {{release_path}} && php bin/console asset-map:compile');
+});
+
+// Hooks
+
+after('deploy:symlink', 'deploy:assets');
+after('deploy:failed', 'deploy:unlock');
